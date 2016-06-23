4b09660bbde5ffae007e772aa4c03763825328af
Pavel Tyslacki <pavel.tyslyatsky@gmail.com>
2016-06-23 20:59:39 +0300

    add exception handler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import logging
+import logging.handlers
 import urllib.parse
 
 from flask import Flask, request, redirect, url_for, render_template, Response
@@ -8,16 +8,17 @@ from werkzeug.contrib.atom import AtomFeed
 from parser import parse_topic, parse_icon
 
 
-BASE = 'http://forum.onliner.by'
-MAX_ITEMS = 20
-CACHE_TYPE = 'simple'
-
-
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.logger.addHandler(logging.StreamHandler())
-app.logger.setLevel(logging.INFO)
+app.config.from_object('settings')
 cache = Cache(app)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+app.logger.addHandler(stream_handler)
+
+smtp_handler = logging.handlers.SMTPHandler(**app.config['ERROR_EMAIL'])
+smtp_handler.setLevel(logging.ERROR)
+app.logger.addHandler(smtp_handler)
 
 
 def _args_cache_key():
@@ -36,7 +37,7 @@ def home():
 @app.route('/feed.atom')
 @cache.cached(timeout=5 * 60, key_prefix=_args_cache_key)
 def feed():
-    title, messages = parse_topic(BASE, request.args.get('url'), MAX_ITEMS)
+    title, messages = parse_topic(app.config.get('BASE'), request.args.get('url'), app.config.get('MAX_ITEMS'))
     feed = AtomFeed(title, feed_url=request.url, url=request.url_root, icon=url_for('favicon'))
     for message in messages:
         feed.add(**message)
@@ -46,4 +47,4 @@ def feed():
 @app.route('/favicon.ico')
 @cache.cached(timeout=24 * 60 * 60)
 def favicon():
-    return Response(**parse_icon(BASE))
+    return Response(**parse_icon(app.config.get('BASE')))
---
+++ settings.py
@@ -0,0 +1,12 @@
+BASE = 'http://forum.onliner.by'
+MAX_ITEMS = 20
+CACHE_TYPE = 'simple'
+
+ERROR_EMAIL = {
+    'mailhost': ('', ''),
+    'fromaddr': '',
+    'toaddrs': [''],
+    'subject': 'Onliner RSS error',
+    'credentials': ('', ''),
+    'secure': (),
+}
